f86b5419772848eb9f06c524c35d7543015b1d72
ying-qi-wei <774599093@qq.com>
2019-09-25 16:13:58 +0800

    Maven程序实例“ git commit -m Maven程序实例“

---
+++ myboot/src/main/java/com/hunau/myboot/controller/TemplateController.java
@@ -0,0 +1,13 @@
+package com.hunau.myboot.controller;/**
+ * @Description:
+ * @param $
+ * @Return: $
+ * @开发人员: 黄娟
+ * @单位：湖南农业大学
+ * @Date: $ $
+ */
+*@开发版本：综合练习Vo.1
+        */
+
+public class TemplateController {
+}
